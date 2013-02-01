24802f52b692bd0d463435d3ab7e22ca87d55e5b
shuaqiu <qiushaohua@yahoo.cn>
2013-02-02 00:35:27 +0800

    add problem 4

--- py/challenge/challenge.py
+++ py/challenge/challenge.py
@@ -4,11 +4,11 @@
 #
 
 
-def prob1():
+def prob0():
     return 2 << 37
 
 
-def prob2():
+def prob1():
     def trans(s, shift):
         t = ""
         for c in s:
@@ -37,7 +37,7 @@ def get_content(page):
     return sio.readlines()
 
 
-def prob3():
+def prob2():
     lines = get_content("/pc/def/ocr.html")
     if lines is None:
         return (False, "could not read the content")
@@ -50,7 +50,7 @@ def prob3():
     return res
 
 
-def prob4():
+def prob3():
     lines = get_content("/pc/def/equality.html")
     if lines is None:
         return (False, "not content")
@@ -61,3 +61,15 @@ def prob4():
     found = re.findall(reg, contents)
 
     return "".join([s[4] for s in found])
+
+
+def prob4(nextval="12345"):
+    import urllib2
+    import re
+
+    for i in xrange(400):
+        f = urllib2.urlopen("http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=" + nextval)
+        data = f.read()
+        print i, nextval, data
+
+        nextval = re.findall(r"\d+", data)[0]
