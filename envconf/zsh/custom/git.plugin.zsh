fba2229886447f4312e52535504082e554fddd8d
shuaqiu <qiushaohua.bp@gmail.com>
2013-06-20 20:28:03 +0800

    add alias to show git log pretty

---
+++ envconf/zsh/custom/git.plugin.zsh
@@ -0,0 +1,2 @@
+lg="log --graph --abbrev-commit --decorate --date=local --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%ad%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset) %C(dim white)- %an%C(reset)%n            %C(white)%s%C(reset)%n' --all"
+alias glog="git $lg"
