3f4e22241275a753fbd15e495cd5ff273e98a46a
shuaqiu <qiushaohua.bp@gmail.com>
2019-03-31 10:43:46 +0800

    add flutter china env setting; change the exported path

---
+++ envconf/zsh/custom/flutter-china-env.zsh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export PUB_HOSTED_URL=https://pub.flutter-io.cn
+export FLUTTER_STORAGE_BASE_URL=https://storage.flutter-io.cn
+
--- envconf/zsh/custom/path-export.zsh
+++ envconf/zsh/custom/path-export.zsh
@@ -1,11 +1,6 @@
 #!/bin/zsh
 
 ## export the 
-OPT=$HOME/opt
 
-GRADLE=$OPT/gradle/bin
-SCALA=$OPT/scala/bin
-TOMCAT=$OPT/apache-tomcat/bin
-
-export PATH=$GRADLE:$SCALA:$TOMCAT:$PATH
+export PATH=$HOME/.local/bin:$PATH
 
